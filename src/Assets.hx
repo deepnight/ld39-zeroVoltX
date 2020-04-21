7fa1f4ada9bba9c46991587e5863d41c625be61b
wayne <molockree@gmail.com>
2020-04-21 21:26:45 +0800

    fixed broken dependency from dn.heaps in Assets.hx file

--- src/Assets.hx
+++ src/Assets.hx
@@ -1,14 +1,14 @@
 import dn.heaps.Sfx;
 
 class Assets {
-	public static var SBANK = Sfx.importDirectory("sfx");
+	public static var SBANK = dn.heaps.assets.SfxDirectory.load("sfx");
 	public static var font : h2d.Font;
 	public static var tiles : SpriteLib;
 	public static var music : dn.heaps.Sfx;
 
 	public static function init() {
 		font = hxd.Res.minecraftiaOutline.toFont();
-		tiles = dn.heaps.slib.assets.Atlas.load("tiles.atlas");
+		tiles = dn.heaps.assets.Atlas.load("tiles.atlas");
 
 		Sfx.setGroupVolume(0,1);
 		Sfx.setGroupVolume(1,0.55);
